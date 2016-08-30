53db5eb56482f849253071ca8f29f39eaedee5e7
Azamat Kurbanaev <azamat.kurbanaev@arcadia.spb.ru>
2016-08-30 18:09:22 +0300

    logginf os post

--- src/android/com/synconset/CordovaHTTP/CordovaHttpPostJson.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttpPostJson.java
@@ -31,6 +31,9 @@ public class CordovaHttpPostJson extends CordovaHttp implements Runnable {
             request.headers(this.getHeaders());
             request.acceptJson();
             request.contentType(HttpRequest.CONTENT_TYPE_JSON);
+            
+            Log.d(getJsonObject().toString());
+
             request.send(getJsonObject().toString());
             int code = request.code();
             String body = request.body(CHARSET);
